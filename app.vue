526a27307004d7a49be38afafaa30e3e00e8b062
dev11 <dev11@webteamwork.ca>
2025-06-11 12:35:04 +0330

    Add Tailwind

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="min-h-screen bg-yellow-200 flex items-center justify-center">
+    <h1 class="text-5xl font-bold text-yellow-800">Tailwind CSS is working!</h1>
   </div>
 </template>
+
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+	postcss: {
+		plugins: {
+			'postcss-import': {},
+			"tailwindcss/nesting": {},
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	},
+	css: ['~/assets/css/tailwind.css'],
+  devtools: { enabled: true },
 })
